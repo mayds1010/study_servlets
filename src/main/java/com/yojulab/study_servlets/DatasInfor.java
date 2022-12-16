4e9cf7d5f643b6ab7ea227f69982d39412db7a22
mayds1010 <mayds1010@gmail.com>
2022-12-16 11:19:24 +0900

    commit

--- src/main/java/com/yojulab/study_servlets/DatasInfor.java
+++ src/main/java/com/yojulab/study_servlets/DatasInfor.java
@@ -3,6 +3,8 @@ package com.yojulab.study_servlets;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import com.yojulab.beans.MemberBean;
+
 public class DatasInfor {
 
     // search 인풋
@@ -35,6 +37,42 @@ public class DatasInfor {
         // 키는 다른이름 가능
         bundlesData.put("searchForm", searchForm);
         bundlesData.put("tablesListWithString", tablesListWithString);
+
+        bundlesData.put("dataWithMemberBean",datasInfor.getDataWithMemberBean());
+        bundlesData.put("dataListWithMemberBean",datasInfor.getDataListWithMemberBean());
+
         return bundlesData;
     }
+
+    public MemberBean getDataWithMemberBean(){
+        MemberBean memberBean = new MemberBean();
+        memberBean.setFirstName("Mark");
+        memberBean.setSecondName("Otto");
+        memberBean.setHandleName("@mdo");
+
+        return memberBean;
+    }
+
+ public ArrayList<MemberBean> getDataListWithMemberBean(){
+    ArrayList<MemberBean> membersList = new ArrayList<>(); 
+    MemberBean memberBean = new MemberBean();
+    memberBean.setFirstName("Mark");
+    memberBean.setSecondName("Otto");
+    memberBean.setHandleName("@mdo");
+    membersList.add(memberBean);
+
+    MemberBean memberBean02 = new MemberBean();
+    memberBean02.setFirstName("jacob");
+    memberBean02.setSecondName("Thornton");
+    memberBean02.setHandleName("@mfat");
+    membersList.add(memberBean02);
+
+    MemberBean memberBean03 = new MemberBean();
+    memberBean03.setFirstName("Larry");
+    memberBean03.setSecondName("Bird");
+    memberBean03.setHandleName("@twitter");
+    membersList.add(memberBean03);
+
+    return membersList;
+ }
 }
