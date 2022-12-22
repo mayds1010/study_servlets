627e41cd984bc14315bb329722fa46c131c827dd
mayds1010 <mayds1010@gmail.com>
2022-12-22 10:29:15 +0900

    commit

--- src/main/java/com/yojulab/study_servlets/dao/PollWithDB.java
+++ src/main/java/com/yojulab/study_servlets/dao/PollWithDB.java
@@ -1,28 +1,29 @@
 package com.yojulab.study_servlets.dao;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.HashMap;
-import java.sql.SQLException;
-
-
 
 public class PollWithDB {
-public void getQuestion(String questionUid) throws SQLException{
-  
+    public HashMap<String, Object> getQuestion(String questionsUid) throws SQLException{
+
         Commons commons = new Commons();
-        Statement statement = commons.getStatement(); 
-  
-        String query = "SELECT * FROM QUESTIONS_LIST" +    
-                         "WHERE QUESTIONS_UID = '"+questionUid+"'";              
-   
+        Statement statement = commons.getStatement();
+
+        String query = "SELECT * FROM QUESTIONS_LIST " +
+                        " WHERE QUESTIONS_UID = '"+questionsUid+"'";
+        
         ResultSet resultSet = statement.executeQuery(query);
-        HashMap<String,Object> result;
-        while(resultSet.next());    
-         resultSet.getString("QUESTIONS_UID");
-         resultSet.getString("QUESTIONS");
-         resultSet.getString("ORDERS");
-         
+        HashMap<String, Object> result = null;
+        while(resultSet.next()){
+            result = new HashMap<>();
+            result.put("QUESTIONS_UID",resultSet.getString("QUESTIONS_UID"));
+            result.put("QUESTIONS", resultSet.getString("QUESTIONS"));
+            result.put("ORDERS",resultSet.getInt("ORDERS"));
+        }
+
+        return result;
     }
 }
 
